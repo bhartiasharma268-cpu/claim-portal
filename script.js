637b1060bfe643f0f36f1858ac3f1ad8b9bfa8f2
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-08 16:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,9 @@ function addClaim(){
 var claim=document.getElementById("claim").value;
 
 if(claim==""){
-alert("Please enter claim");
+alert("Enter claim");
 return;
 }
-
-var list=document.getElementById("claimList");
-
-var li=document.createElement("li");
-
-li.innerHTML = claim + " <button onclick='deleteClaim(this)'>Delete</button>";
-
-list.appendChild(li);
-
-document.getElementById("claim").value="";
-
-}
-
-function deleteClaim(btn){
-
-btn.parentElement.remove();
-
-}
-
-function addClaim(){
-
-var claim=document.getElementById("claim").value;
 
 var list=document.getElementById("claimList");
 
@@ -39,20 +17,22 @@ list.appendChild(li);
 
 document.getElementById("claim").value="";
 
+updateTotal();
+
 }
 
 function deleteClaim(btn){
 
 btn.parentElement.remove();
 
+updateTotal();
+
 }
 
-function showPage(page){
+function updateTotal(){
 
-document.getElementById("home").style.display="none";
-document.getElementById("claims").style.display="none";
-document.getElementById("about").style.display="none";
+var total=document.getElementById("claimList").children.length;
 
-document.getElementById(page).style.display="block";
+document.getElementById("total").innerText=total;
 
 }
