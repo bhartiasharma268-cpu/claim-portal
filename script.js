acada8f73ad4df2629b068bd7af08fadf121d67f
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-08 16:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ function deleteClaim(btn){
 btn.parentElement.remove();
 
 }
+
+function addClaim(){
+
+var claim=document.getElementById("claim").value;
+
+var list=document.getElementById("claimList");
+
+var li=document.createElement("li");
+
+li.innerHTML=claim+" <button onclick='deleteClaim(this)'>Delete</button>";
+
+list.appendChild(li);
+
+document.getElementById("claim").value="";
+
+}
+
+function deleteClaim(btn){
+
+btn.parentElement.remove();
+
+}
+
+function showPage(page){
+
+document.getElementById("home").style.display="none";
+document.getElementById("claims").style.display="none";
+document.getElementById("about").style.display="none";
+
+document.getElementById(page).style.display="block";
+
+}
