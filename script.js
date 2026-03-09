b577e3a3f9cd0ad4232b2be66a703ba3f04394b5
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-10 01:50:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function addClaim(){
+// Dashboard Data
 
-var claim=document.getElementById("claim").value;
+let pnl = 36433.16
+let winRate = 58.55
+let profitFactor = 2.11
+let rr = 1.49
 
-if(claim==""){
-alert("Enter claim name");
-return;
+// Show values on dashboard
+
+document.getElementById("pnl").innerText = "$" + pnl
+document.getElementById("winrate").innerText = winRate + "%"
+document.getElementById("profitfactor").innerText = profitFactor
+document.getElementById("rr").innerText = rr
+
+
+// Weekly Trade Distribution Chart
+
+const ctx = document.getElementById('tradeChart')
+
+new Chart(ctx, {
+type: 'bar',
+data: {
+labels: ["Monday","Tuesday","Wednesday","Thursday","Friday"],
+datasets: [
+{
+label: "Winning Trades",
+data: [25,13,28,16,31],
+backgroundColor: "green"
+},
+{
+label: "Losing Trades",
+data: [14,18,16,13,19],
+backgroundColor: "red"
 }
-
-var list=document.getElementById("claimList");
-
-var li=document.createElement("li");
-
-li.innerHTML=claim;
-
-list.appendChild(li);
-
-document.getElementById("claim").value="";
-
-updateTotal();
-
+]
 }
-
-function updateTotal(){
-
-var total=document.getElementById("claimList").children.length;
-
-document.getElementById("total").innerText=total;
-
-}
+})
