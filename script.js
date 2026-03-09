ab0609a3b9d2524f4a9b888f9c106df69c55e2ac
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-10 01:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Dashboard Data
+// Dashboard values
 
 let pnl = 36433.16
 let winRate = 58.55
 let profitFactor = 2.11
 let rr = 1.49
-
-// Show values on dashboard
 
 document.getElementById("pnl").innerText = "$" + pnl
 document.getElementById("winrate").innerText = winRate + "%"
@@ -13,25 +11,34 @@ document.getElementById("profitfactor").innerText = profitFactor
 document.getElementById("rr").innerText = rr
 
 
-// Weekly Trade Distribution Chart
+// Chart
 
 const ctx = document.getElementById('tradeChart')
 
 new Chart(ctx, {
+
 type: 'bar',
+
 data: {
+
 labels: ["Monday","Tuesday","Wednesday","Thursday","Friday"],
+
 datasets: [
+
 {
-label: "Winning Trades",
-data: [25,13,28,16,31],
-backgroundColor: "green"
+label:"Winning Trades",
+data:[25,13,28,16,31],
+backgroundColor:"green"
 },
+
 {
-label: "Losing Trades",
-data: [14,18,16,13,19],
-backgroundColor: "red"
+label:"Losing Trades",
+data:[14,18,16,13,19],
+backgroundColor:"red"
 }
+
 ]
+
 }
+
 })
