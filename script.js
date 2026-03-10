1426cf776d8d59b9e1effcbca490abc014fe677a
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-10 11:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,30 @@
-let revenueChart;
-let profitChart;
-let productChart;
+let ctx = document.getElementById("salesChart")
 
-function createCharts(){
-
-profitChart = new Chart(document.getElementById("profitChart"),{
-
-type:"pie",
-
-data:{
-labels:["Samsung","Vivo"],
-datasets:[{
-data:[15,8],
-backgroundColor:["#caa400","#4b4bff"]
-}]
-}
-
-});
-
-
-revenueChart = new Chart(document.getElementById("revenueChart"),{
-
-type:"doughnut",
-
-data:{
-labels:["Samsung","Vivo"],
-datasets:[{
-data:[55,30],
-backgroundColor:["#caa400","#4b4bff"]
-}]
-}
-
-});
-
-
-productChart = new Chart(document.getElementById("productChart"),{
+new Chart(ctx,{
 
 type:"bar",
 
 data:{
-labels:["Vivo X","Vivo T","Galaxy S","Galaxy Fold","Vivo Y","Galaxy M"],
+
+labels:["Samsung","Vivo"],
+
 datasets:[{
-label:"Units Sold",
-data:[85,80,78,75,70,68],
-backgroundColor:"#4b4bff"
+
+label:"Revenue",
+
+data:[55,30],
+
+backgroundColor:["gold","blue"]
+
 }]
-}
-
-});
 
 }
 
-createCharts();
+})
 
 
-function updateDashboard(){
+function darkMode(){
 
-let company = document.getElementById("companyFilter").value;
-
-if(company=="Samsung"){
-
-document.getElementById("revenueKPI").innerText="55B";
-document.getElementById("profitKPI").innerText="15B";
-document.getElementById("unitsKPI").innerText="280";
-document.getElementById("ratingKPI").innerText="4.2";
-
-}
-
-else if(company=="Vivo"){
-
-document.getElementById("revenueKPI").innerText="30B";
-document.getElementById("profitKPI").innerText="8B";
-document.getElementById("unitsKPI").innerText="240";
-document.getElementById("ratingKPI").innerText="4.4";
-
-}
-
-else{
-
-document.getElementById("revenueKPI").innerText="85B";
-document.getElementById("profitKPI").innerText="23B";
-document.getElementById("unitsKPI").innerText="520";
-document.getElementById("ratingKPI").innerText="4.3";
-
-}
+document.body.classList.toggle("dark")
 
 }
