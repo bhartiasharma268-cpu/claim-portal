538c4d9d36b014abb53c72605e5c12a65ae3852f
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-10 10:59:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
-// Dashboard values
-
-let pnl = 36433.16
-let winRate = 58.55
-let profitFactor = 2.11
-let rr = 1.49
-
-document.getElementById("pnl").innerText = "$" + pnl
-document.getElementById("winrate").innerText = winRate + "%"
-document.getElementById("profitfactor").innerText = profitFactor
-document.getElementById("rr").innerText = rr
-
-
-// Chart
-
-const ctx = document.getElementById('tradeChart')
-
-new Chart(ctx, {
-
-type: 'bar',
-
-data: {
-
-labels: ["Monday","Tuesday","Wednesday","Thursday","Friday"],
-
-datasets: [
-
+// Purchase Reason Chart
+new Chart(document.getElementById("reasonChart"),{
+type:"bar",
+data:{
+labels:["Gaming","Camera","Premium Design","Battery","Value"],
+datasets:[
 {
-label:"Winning Trades",
-data:[25,13,28,16,31],
-backgroundColor:"green"
+label:"Samsung",
+data:[40,35,30,25,20],
+backgroundColor:"gold"
 },
-
 {
-label:"Losing Trades",
-data:[14,18,16,13,19],
-backgroundColor:"red"
+label:"Vivo",
+data:[50,45,40,35,30],
+backgroundColor:"blue"
 }
-
 ]
-
 }
+});
 
-})
+// Profit Pie Chart
+new Chart(document.getElementById("profitChart"),{
+type:"pie",
+data:{
+labels:["Samsung","Vivo"],
+datasets:[{
+data:[15,8],
+backgroundColor:["gold","blue"]
+}]
+}
+});
+
+// Rating Chart
+new Chart(document.getElementById("ratingChart"),{
+type:"bar",
+data:{
+labels:["Vivo X","Vivo T","Galaxy S","Galaxy Fold","Vivo Y"],
+datasets:[{
+label:"Customer Rating",
+data:[85,80,78,76,70],
+backgroundColor:"purple"
+}]
+}
+});
+
+// Revenue Donut Chart
+new Chart(document.getElementById("revenueChart"),{
+type:"doughnut",
+data:{
+labels:["Samsung","Vivo"],
+datasets:[{
+data:[55,30],
+backgroundColor:["gold","blue"]
+}]
+}
+});
+
+// Product Sales Chart
+new Chart(document.getElementById("productChart"),{
+type:"bar",
+data:{
+labels:["Vivo X","Vivo T","Galaxy S","Galaxy Fold","Vivo Y","Galaxy M"],
+datasets:[{
+label:"Units Sold",
+data:[85,80,78,75,70,68],
+backgroundColor:"blue"
+}]
+}
+});
