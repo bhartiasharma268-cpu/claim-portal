2c6fb277616dd7562568fd9cff3dcdf08a21b2c2
bhartiasharma268-cpu <bhartiasharma268@gmail.com>
2026-03-10 11:09:36 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
+function login(){
 
+let user = document.getElementById("username").value
+let pass = document.getElementById("password").value
+
+if(user=="admin" && pass=="1234"){
+
+window.location="dashboard.html"
+
+}
+
+else{
+
+alert("Wrong Login")
+
+}
+
+}
